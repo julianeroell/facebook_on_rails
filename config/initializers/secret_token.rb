c36dfed0aecc6da8dfb9ceb671908452a35711c4
Martin Roell <martin@roell.net>
2013-12-21 12:30:15 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FacebookOnRails::Application.config.secret_key_base = '97304517b7d20ef2ad9ea6465ed9a27e31d2b186e7668f49c433631eabc6c4b47a14bdb3032633345f0d021c6e54354669a2f740042abcfa1189d46fc6374701'
